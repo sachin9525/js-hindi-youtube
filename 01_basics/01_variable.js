e91fa343f336ab14540a7bf36a0474b8de8c665d
sachin9525 <145637427+sachin9525@users.noreply.github.com>
2024-01-05 13:42:54 +0000

    variable config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 1526
+let accountEmail = "Sachin@gmail.com"
+var accountPassword = "78946123"
+accountCity = "Bhopal"
+let accountState;
+
+// accountId = 1; // not allowed
+console.log(accountId); 
+
+accountEmail = "sachhi@gmail.com"
+accountPassword = "112233"
+accountCity = "patna"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
