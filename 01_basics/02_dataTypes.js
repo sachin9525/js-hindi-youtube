c723d0d13fb59516c8b240272756e88fd7e6e71b
Sachin Kumar <sachinkumar952577@gmail.com>
2024-01-06 17:04:30 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+                            // DataTypes in js
+// number => 2 to power 53
+
+// bigint
+
+// string => ""
+
+// boolean => true/false
+
+// null => standalone value
+
+// undefined
+
+// symbol => unique
+
+// object
+
+
+// console.log(typeof null) // object
+// console.log(typeof undefined); // undefined
+
+"use strict"; // treat all js code as newer version
+
+alert(3 + 3) // we are using node js, not browser
+
