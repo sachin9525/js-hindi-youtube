ef3f059481d571e0081841318192c94b735a9ba1
Sachin Kumar <sachinkumar952577@gmail.com>
2024-01-18 17:45:37 +0530

    Numbers & math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+//  const score = 400;
+//  console.log(score);
+
+//  const blance = new Number(100);
+//  console.log(blance);
+
+//  console.log(blance.toString().length);
+//  console.log(blance.toFixed(2));
+
+//  const otherNumber =  123.8466
+//  console.log(otherNumber.toPrecision(5));
+
+//  const hundreds = 1000000;
+//  console.log(hundreds.toLocaleString("en-IN"));
+
+// ********************** Maths *************************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2));   // Max value
+// console.log(Math.floor(4.9));  // Min value
+// console.log(Math.min(4, 3, 8, 6))
+// console.log(Math.max(4, 3, 8, 6))
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);  
+console.log(Math.floor(Math.random()*10) + 1);  
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+ 
+
+
+
